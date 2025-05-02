b3e804b00e64056c8599b5f1e32d0fa36cb02f5a
MasaruArai <masaru_arai@wao-corp.com>
2025-05-02 11:00:49 +0900

    初期化場所修正

--- platform/android/java/lib/src/org/godotengine/godot/Godot.java
+++ platform/android/java/lib/src/org/godotengine/godot/Godot.java
@@ -222,9 +222,6 @@ public class Godot extends Fragment implements SensorEventListener, IDownloaderC
 		 */
 		@Nullable
 		protected View onMainCreateView(Activity activity) {
-			int currentNightMode = getResources().getConfiguration().uiMode
-                & Configuration.UI_MODE_NIGHT_MASK;
-        	darkMode = currentNightMode == Configuration.UI_MODE_NIGHT_YES;
 			return null;
 		}
 
@@ -845,6 +842,9 @@ public class Godot extends Fragment implements SensorEventListener, IDownloaderC
 		}
 
 		mCurrentIntent = activity.getIntent();
+
+		int currentNightMode = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        darkMode = currentNightMode == Configuration.UI_MODE_NIGHT_YES;
 
 		initializeGodot();
 		BenchmarkUtils.endBenchmarkMeasure("Godot::onCreate");
